ef0dc2b3823bf2e86c9fe8df0c7004f15efd55c4
shinys10 <166797095+sysys10@users.noreply.github.com>
2025-05-21 17:49:41 +0900

    Update SidebarList.tsx

--- apps/client/src/components/layout/LeftSidebar/SidebarList.tsx
+++ apps/client/src/components/layout/LeftSidebar/SidebarList.tsx
@@ -20,7 +20,7 @@ export function SidebarList({
     if (user?.userId) {
       handleGetRecentQuestion()
     }
-  }, [user])
+  }, [])
 
   const handleAllRecentQuestionRemove = async () => {
     handleRemoveRecentQuestion({
