2405c779fe0a8e217b6ac6cee224adf4207ef30b
shinys10 <166797095+sysys10@users.noreply.github.com>
2025-05-28 14:13:04 +0900

    Update useLoginMutation.ts

--- apps/client/src/hooks/query/useLoginMutation.ts
+++ apps/client/src/hooks/query/useLoginMutation.ts
@@ -16,6 +16,7 @@ const useLoginMutation = () => {
     },
     onSuccess: (data) => {
       if (!data.success) {
+        console.log(data)
         throw new Error({ message: data.message, status:400} as unknown as string)
       }
       setHeader(data.body.jwtToken)
